348017c6562ae939a2236a7362456098ee09eb52
AmanBarbuddhe <amanbarbuddhe9@gmail.com>
2025-12-27 15:38:04 +0530

    Day 2 folder added

---
+++ Day2/Operator.java
@@ -0,0 +1,18 @@
+public class Operator {
+    
+    public static void main(String args[]){
+
+        int a = 3 ;
+        int b = 5 ;
+
+        System.out.println("Answer : " + a + b);
+
+        System.out.println("Answer : " + (a+b));
+
+        System.out.println(a + b + ":Answer");
+
+        System.out.println(a+ +b);
+
+    }
+
+}
