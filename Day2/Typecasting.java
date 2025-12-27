92016004c0a48db21822f01e4c7cd4b92c1bee33
AmanBarbuddhe <amanbarbuddhe9@gmail.com>
2025-12-27 16:43:40 +0530

    Day 2 folder added

---
+++ Day2/Typecasting.java
@@ -0,0 +1,15 @@
+public class Typecasting {
+    public static void main(String args[]){
+
+        //Explicit Typecasting
+        int a = (int)29.37;
+        System.out.println(a);
+
+        //Implicit Typecasting
+        float b = 12;
+        System.out.println(b);
+
+
+
+    }
+}
